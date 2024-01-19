f0b6b886b70bf65e138295955cccd876630edae9
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2024-01-19 17:05:52 -0300

    Salvando a seleção de tema 'dark' 'light' no localstorage

--- src/shared/contexts/ThemeContext.tsx
+++ src/shared/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useContext, useMemo, useState } from 'react'
+import React, { createContext, useCallback, useContext, useMemo, useState, useEffect } from 'react'
 import { ThemeProvider } from '@emotion/react'
 import { DarkTheme, LightTheme } from './../themes'
 import { Box } from '@mui/material'
@@ -19,27 +19,30 @@ interface IAppThemeProviderProps {
 }
 
 export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({ children }) => {
-    const [themeName, setThemeName] = useState<'light' | 'dark'>('light')
+    const [themeName, setThemeName] = useState<'light' | 'dark'>(
+        () => (localStorage.getItem('theme') as 'light' | 'dark') || 'light'
+    )
 
     const toggleTheme = useCallback(() => {
-        setThemeName(oldThemeName => (oldThemeName === 'light') ? 'dark' : 'light')
+        setThemeName((oldThemeName) => (oldThemeName === 'light' ? 'dark' : 'light'))
     }, [])
 
-    const theme = useMemo(() => {
-        if (themeName === 'light') return LightTheme
+    useEffect(() => {
+        // Salvar o tema no LocalStorage sempre que ele for alterado
+        localStorage.setItem('theme', themeName)
+    }, [themeName])
 
-        return DarkTheme
+    const theme = useMemo(() => {
+        return themeName === 'light' ? LightTheme : DarkTheme
     }, [themeName])
 
     return (
         <ThemeContext.Provider value={{ themeName, toggleTheme }}>
             <ThemeProvider theme={theme}>
-                <Box width='100%' height='100vh' bgcolor={theme.palette.background.default}>
+                <Box width="100%" height="100vh" bgcolor={theme.palette.background.default}>
                     {children}
                 </Box>
             </ThemeProvider>
         </ThemeContext.Provider>
     )
 }
-
-
