245acc7e0d054357752d0a4f6fc5043aceb6c4a2
Lazaro Henrique <lazarohenriquefernandeslima@hotmail.com>
2024-02-21 19:33:38 -0300

    Ajsutando a validação de email

--- src/pages/Public/ForgotPassword/validation/Schema.ts
+++ src/pages/Public/ForgotPassword/validation/Schema.ts
@@ -6,5 +6,5 @@ export interface IFormDataForgotPassword {
 
 //Definindo o schema para validação
 export const formatValidationSchemaForgotPassword: yup.Schema<IFormDataForgotPassword> = yup.object().shape({
-    email: yup.string().required().email().min(5).max(100),
+    email: yup.string().email().min(5).max(100).matches(/^[\w!#$%&'*+/=?`{|}~.-]+@([\w-]+\.)+[\w-]{2,4}$/, 'Ex: exemplo@dominio.com').required(),
 })
--- src/pages/Public/RedefinePassword/validation/Schema.ts
+++ src/pages/Public/RedefinePassword/validation/Schema.ts
@@ -9,7 +9,7 @@ export interface IFormData {
 
 //Definindo o schema para validação de atualização
 export const formatValidationSchema: yup.Schema<IFormData> = yup.object().shape({
-    email: yup.string().required().email().min(5).max(100),
+    email: yup.string().email().min(5).max(100).matches(/^[\w!#$%&'*+/=?`{|}~.-]+@([\w-]+\.)+[\w-]{2,4}$/, 'Ex: exemplo@dominio.com').required(),
     verification_token: yup.string().transform(value => (value ? value.trim() : '')).length(6).required(),
     password: yup.string().required().min(6).max(256),
     confirmPassword: yup.string().oneOf([yup.ref('password')], 'As senhas devem ser iguais').required()
